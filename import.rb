73d7e047f8830a21b65bea774012183ebba654fc
Qing Wu <wiserfirst@gmail.com>
2015-07-27 22:43:15 +1000

    Site updated at: 2015-07-27 12:43:15 UTC

--- import.rb
+++
@@ -1,7 +0,0 @@
-require "jekyll-import"
-
-JekyllImport::Importers::WordpressDotCom.run({
-  "source" => "/Users/wiser/Downloads/peacefulrevolution.wordpress.2015-07-26.xml",
-  "no_fetch_images" => false,
-  "assets_folder" => "assets"
-})
